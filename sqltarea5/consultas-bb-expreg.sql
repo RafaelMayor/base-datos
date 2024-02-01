0dcc18cfe0cf6ac4b15c54a92ff0243d56c559a6
RafaelMayor <rmarmay2004gmail.com>
2024-02-01 20:25:21 +0000

    Update

--- sqltarea5/consultas-bb-expreg.sql
+++ sqltarea5/consultas-bb-expreg.sql
@@ -1,17 +1,15 @@
 -- Selección de libros cuyo título comienza con "H".
 
-/**
 select * from libro where titulo regexp '^H';
-
+/**
 No devuelve nada porque ningún título comienza con "H".
 **/
 
 
 -- Libros escritos por autores cuyos nombres terminan con "ing".
 
-/**
 select * from libro INNER JOIN autor where autor.nombre regexp 'ing$';
-
+/**
 ┌────────┬───────────────────────────────────┬──────────┬─────────────────────────────────────────────┬────────┬────┬──────────────┐
 │ codigo │              titulo               │ autor_id │                  editorial                  │ precio │ id │    nombre    │
 ├────────┼───────────────────────────────────┼──────────┼─────────────────────────────────────────────┼────────┼────┼──────────────┤
@@ -79,8 +77,8 @@ select * from libro INNER JOIN autor where autor.nombre regexp 'ing$';
 
 -- Libros con títulos que contienen la palabra "and" en cualquier posición.
 
+select * from libro where titulo regexp 'and';
 /**
-sqlite> select * from libro where titulo regexp 'and';
 ┌────────┬──────────────────────────────────┬──────────┬─────────────────────────┬────────┐
 │ codigo │              titulo              │ autor_id │        editorial        │ precio │
 ├────────┼──────────────────────────────────┼──────────┼─────────────────────────┼────────┤
@@ -94,8 +92,8 @@ sqlite> select * from libro where titulo regexp 'and';
 
 -- Libros cuyo título comienza con una vocal.
 
+SELECT * FROM libro WHERE titulo REGEXP '^[aeiouAEIOU]';
 /**
-sqlite> SELECT * FROM libro WHERE titulo REGEXP '^[aeiouAEIOU]';
 ┌────────┬──────────────────────────────────┬──────────┬───────────────────────┬────────┐
 │ codigo │              titulo              │ autor_id │       editorial       │ precio │
 ├────────┼──────────────────────────────────┼──────────┼───────────────────────┼────────┤
@@ -108,8 +106,9 @@ sqlite> SELECT * FROM libro WHERE titulo REGEXP '^[aeiouAEIOU]';
 
 -- Libros cuyo autor tiene al menos una vocal repetida.
 
+SELECT * FROM libro INNER JOIN autor WHERE autor.nombre REGEXP '[aeiouAEIOU].*[aeiouAEIOU]';
 /**
-sqlite> SELECT * FROM libro INNER JOIN autor WHERE autor.nombre REGEXP '[aeiouAEIOU].*[aeiouAEIOU]';
+
 ┌────────┬───────────────────────────────────┬──────────┬─────────────────────────────────────────────┬────────┬────┬─────────────────┐
 │ codigo │              titulo               │ autor_id │                  editorial                  │ precio │ id │     nombre      │
 ├────────┼───────────────────────────────────┼──────────┼─────────────────────────────────────────────┼────────┼────┼─────────────────┤
@@ -271,71 +270,204 @@ sqlite> SELECT * FROM libro INNER JOIN autor WHERE autor.nombre REGEXP '[aeiouAE
 
 -- Libros cuyos títulos tienen al menos tres palabras.
 
+SELECT * FROM libro WHERE titulo REGEXP '^([^ ]+ +){2,}[^ ]+$';
 /**
 
+┌────────┬───────────────────────────────────┬──────────┬───────────────────────────┬────────┐
+│ codigo │              titulo               │ autor_id │         editorial         │ precio │
+├────────┼───────────────────────────────────┼──────────┼───────────────────────────┼────────┤
+│ 1      │ The Great Gatsby                  │ 6        │ Charles Scribner's Sons   │ 20.99  │
+│ 2      │ To Kill a Mockingbird             │ 7        │ J.B. Lippincott & Co.     │ 15.95  │
+│ 3      │ The Catcher in the Rye            │ 8        │ Little, Brown and Company │ 18.75  │
+│ 4      │ One Hundred Years of Solitude     │ 9        │ Harper & Row              │ 22.5   │
+│ 5      │ Brave New World                   │ 3        │ Chatto & Windus           │ 17.99  │
+│ 7      │ The Lord of the Rings             │ 10       │ George Allen & Unwin      │ 35.5   │
+│ 8      │ The Chronicles of Narnia          │ 11       │ Geoffrey Bles             │ 28.99  │
+│ 13     │ The Grapes of Wrath               │ 15       │ The Viking Press          │ 21.5   │
+│ 15     │ The Old Man and the Sea           │ 17       │ Charles Scribner's Sons   │ 18.95  │
+│ 16     │ The Count of Monte Cristo         │ 18       │ Pétion                    │ 27.99  │
+│ 17     │ The Picture of Dorian Gray        │ 19       │ Ward, Lock, and Company   │ 14.5   │
+│ 18     │ The Adventures of Sherlock Holmes │ 20       │ George Newnes             │ 16.99  │
+│ 20     │ Alice's Adventures in Wonderland  │ 22       │ Macmillan                 │ 11.5   │
+│ 23     │ The Divine Comedy                 │ 25       │ Dante Alighieri           │ 20.5   │
+│ 26     │ The Jungle Book                   │ 28       │ Macmillan Publishers      │ 14.99  │
+│ 27     │ The Wind in the Willows           │ 29       │ Methuen & Co.             │ 17.5   │
+│ 28     │ War and Peace                     │ 26       │ The Russian Messenger     │ 33.25  │
+│ 29     │ Crime and Punishment              │ 30       │ The Russian Messenger     │ 19.99  │
+└────────┴───────────────────────────────────┴──────────┴───────────────────────────┴────────┘
 **/
 
 
 -- Obtener todos los autores cuyo nombre empieza con la letra "A":
 
+select * from autor where nombre regexp '^A';
 /**
 
+┌────┬─────────────────┐
+│ id │     nombre      │
+├────┼─────────────────┤
+│ 5  │ Agatha Christie │
+└────┴─────────────────┘
 **/
 
 
 -- Seleccionar los libros cuyo título contiene la palabra "SQL":
 
+SELECT * FROM libro WHERE titulo REGEXP 'SQL';
 /**
-
+No devuelve nada porque ningún título contiene la palabra "SQL".
 **/
 
 
 -- Obtener todos los autores cuyo nombre termina con "ez":
 
+SELECT * FROM autor WHERE nombre REGEXP 'ez$';
 /**
-
+No devuelve nada porque ningún nombre termina con "ez".
 **/
 
 
 -- Obtener todos los autores cuyo nombre tiene al menos 5 caracteres:
 
+SELECT * FROM autor WHERE LENGTH(nombre) >= 5;
 /**
 
+┌────┬─────────────────┐
+│ id │     nombre      │
+├────┼─────────────────┤
+│ 1  │ J.K. Rowling    │
+│ 2  │ Stephen King    │
+│ 3  │ George Orwell   │
+│ 4  │ Jane Austen     │
+│ 5  │ Agatha Christie │
+└────┴─────────────────┘
 **/
 
 
 -- Seleccionar los libros cuya editorial es diferente de "EditorialX":
 
+SELECT * FROM libro WHERE editorial NOT REGEXP '\\bEditorialX\\b';
+
+-- Otra manera:
+
+SELECT * from libro where editorial != 'EditorialX';
 /**
 
+┌────────┬───────────────────────────────────┬──────────┬─────────────────────────────────────────────┬────────┐
+│ codigo │              titulo               │ autor_id │                  editorial                  │ precio │
+├────────┼───────────────────────────────────┼──────────┼─────────────────────────────────────────────┼────────┤
+│ 1      │ The Great Gatsby                  │ 6        │ Charles Scribner's Sons                     │ 20.99  │
+│ 2      │ To Kill a Mockingbird             │ 7        │ J.B. Lippincott & Co.                       │ 15.95  │
+│ 3      │ The Catcher in the Rye            │ 8        │ Little, Brown and Company                   │ 18.75  │
+│ 4      │ One Hundred Years of Solitude     │ 9        │ Harper & Row                                │ 22.5   │
+│ 5      │ Brave New World                   │ 3        │ Chatto & Windus                             │ 17.99  │
+│ 6      │ The Hobbit                        │ 10       │ George Allen & Unwin                        │ 24.99  │
+│ 7      │ The Lord of the Rings             │ 10       │ George Allen & Unwin                        │ 35.5   │
+│ 8      │ The Chronicles of Narnia          │ 11       │ Geoffrey Bles                               │ 28.99  │
+│ 9      │ The Odyssey                       │ 12       │ Homer                                       │ 14.95  │
+│ 10     │ The Iliad                         │ 12       │ Homer                                       │ 14.95  │
+│ 11     │ Moby-Dick                         │ 13       │ Harper & Brothers                           │ 19.99  │
+│ 12     │ The Road                          │ 14       │ Alfred A. Knopf                             │ 16.75  │
+│ 13     │ The Grapes of Wrath               │ 15       │ The Viking Press                            │ 21.5   │
+│ 14     │ Wuthering Heights                 │ 16       │ Emily Brontë                                │ 12.99  │
+│ 15     │ The Old Man and the Sea           │ 17       │ Charles Scribner's Sons                     │ 18.95  │
+│ 16     │ The Count of Monte Cristo         │ 18       │ Pétion                                      │ 27.99  │
+│ 17     │ The Picture of Dorian Gray        │ 19       │ Ward, Lock, and Company                     │ 14.5   │
+│ 18     │ The Adventures of Sherlock Holmes │ 20       │ George Newnes                               │ 16.99  │
+│ 19     │ Frankenstein                      │ 21       │ Lackington, Hughes, Harding, Mavor, & Jones │ 13.25  │
+│ 20     │ Alice's Adventures in Wonderland  │ 22       │ Macmillan                                   │ 11.5   │
+│ 21     │ The Prince                        │ 23       │ Niccolò Machiavelli                         │ 10.99  │
+│ 22     │ Don Quixote                       │ 24       │ Francisco de Robles                         │ 26.75  │
+│ 23     │ The Divine Comedy                 │ 25       │ Dante Alighieri                             │ 20.5   │
+│ 24     │ Anna Karenina                     │ 26       │ The Russian Messenger                       │ 23.99  │
+│ 25     │ Les Misérables                    │ 27       │ A. Lacroix, Verboeckhoven & Cie.            │ 29.75  │
+│ 26     │ The Jungle Book                   │ 28       │ Macmillan Publishers                        │ 14.99  │
+│ 27     │ The Wind in the Willows           │ 29       │ Methuen & Co.                               │ 17.5   │
+│ 28     │ War and Peace                     │ 26       │ The Russian Messenger                       │ 33.25  │
+│ 29     │ Crime and Punishment              │ 30       │ The Russian Messenger                       │ 19.99  │
+└────────┴───────────────────────────────────┴──────────┴─────────────────────────────────────────────┴────────┘
 **/
 
 
 -- Obtener todos los autores cuyo nombre contiene al menos una vocal:
 
+select * from autor where nombre regexp '[aeiou]';
 /**
-
+┌────┬─────────────────┐
+│ id │     nombre      │
+├────┼─────────────────┤
+│ 1  │ J.K. Rowling    │
+│ 2  │ Stephen King    │
+│ 3  │ George Orwell   │
+│ 4  │ Jane Austen     │
+│ 5  │ Agatha Christie │
+└────┴─────────────────┘
 **/
 
 
 -- Seleccionar los libros cuyo título comienza con una letra mayúscula:
 
+select * from libro where titulo regexp '^[A-Z]';
 /**
-
+┌────────┬───────────────────────────────────┬──────────┬─────────────────────────────────────────────┬────────┐
+│ codigo │              titulo               │ autor_id │                  editorial                  │ precio │
+├────────┼───────────────────────────────────┼──────────┼─────────────────────────────────────────────┼────────┤
+│ 1      │ The Great Gatsby                  │ 6        │ Charles Scribner's Sons                     │ 20.99  │
+│ 2      │ To Kill a Mockingbird             │ 7        │ J.B. Lippincott & Co.                       │ 15.95  │
+│ 3      │ The Catcher in the Rye            │ 8        │ Little, Brown and Company                   │ 18.75  │
+│ 4      │ One Hundred Years of Solitude     │ 9        │ Harper & Row                                │ 22.5   │
+│ 5      │ Brave New World                   │ 3        │ Chatto & Windus                             │ 17.99  │
+│ 6      │ The Hobbit                        │ 10       │ George Allen & Unwin                        │ 24.99  │
+│ 7      │ The Lord of the Rings             │ 10       │ George Allen & Unwin                        │ 35.5   │
+│ 8      │ The Chronicles of Narnia          │ 11       │ Geoffrey Bles                               │ 28.99  │
+│ 9      │ The Odyssey                       │ 12       │ Homer                                       │ 14.95  │
+│ 10     │ The Iliad                         │ 12       │ Homer                                       │ 14.95  │
+│ 11     │ Moby-Dick                         │ 13       │ Harper & Brothers                           │ 19.99  │
+│ 12     │ The Road                          │ 14       │ Alfred A. Knopf                             │ 16.75  │
+│ 13     │ The Grapes of Wrath               │ 15       │ The Viking Press                            │ 21.5   │
+│ 14     │ Wuthering Heights                 │ 16       │ Emily Brontë                                │ 12.99  │
+│ 15     │ The Old Man and the Sea           │ 17       │ Charles Scribner's Sons                     │ 18.95  │
+│ 16     │ The Count of Monte Cristo         │ 18       │ Pétion                                      │ 27.99  │
+│ 17     │ The Picture of Dorian Gray        │ 19       │ Ward, Lock, and Company                     │ 14.5   │
+│ 18     │ The Adventures of Sherlock Holmes │ 20       │ George Newnes                               │ 16.99  │
+│ 19     │ Frankenstein                      │ 21       │ Lackington, Hughes, Harding, Mavor, & Jones │ 13.25  │
+│ 20     │ Alice's Adventures in Wonderland  │ 22       │ Macmillan                                   │ 11.5   │
+│ 21     │ The Prince                        │ 23       │ Niccolò Machiavelli                         │ 10.99  │
+│ 22     │ Don Quixote                       │ 24       │ Francisco de Robles                         │ 26.75  │
+│ 23     │ The Divine Comedy                 │ 25       │ Dante Alighieri                             │ 20.5   │
+│ 24     │ Anna Karenina                     │ 26       │ The Russian Messenger                       │ 23.99  │
+│ 25     │ Les Misérables                    │ 27       │ A. Lacroix, Verboeckhoven & Cie.            │ 29.75  │
+│ 26     │ The Jungle Book                   │ 28       │ Macmillan Publishers                        │ 14.99  │
+│ 27     │ The Wind in the Willows           │ 29       │ Methuen & Co.                               │ 17.5   │
+│ 28     │ War and Peace                     │ 26       │ The Russian Messenger                       │ 33.25  │
+│ 29     │ Crime and Punishment              │ 30       │ The Russian Messenger                       │ 19.99  │
+└────────┴───────────────────────────────────┴──────────┴─────────────────────────────────────────────┴────────┘
 **/
 
 
 -- Obtener todos los autores cuyo nombre no contiene la letra "r":
 
+select * from autor where nombre not regexp 'r';
 /**
-
+┌────┬──────────────┐
+│ id │    nombre    │
+├────┼──────────────┤
+│ 1  │ J.K. Rowling │
+│ 2  │ Stephen King │
+│ 4  │ Jane Austen  │
+└────┴──────────────┘
 **/
 
 
 -- Seleccionar los libros cuya editorial empieza con la letra "P":
 
+SELECT * FROM libro WHERE editorial REGEXP '^P';
 /**
-
+┌────────┬───────────────────────────┬──────────┬───────────┬────────┐
+│ codigo │          titulo           │ autor_id │ editorial │ precio │
+├────────┼───────────────────────────┼──────────┼───────────┼────────┤
+│ 16     │ The Count of Monte Cristo │ 18       │ Pétion    │ 27.99  │
+└────────┴───────────────────────────┴──────────┴───────────┴────────┘
 **/
 
 
