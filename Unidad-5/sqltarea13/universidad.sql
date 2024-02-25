63488916d7ecdb0741594a699432e2939368eb3f
RafaelMayor <rmarmay2004@gmail.com>
2024-02-25 03:38:40 +0000

    Update

--- Unidad-5/sqltarea13/universidad.sql
+++ Unidad-5/sqltarea13/universidad.sql
@@ -39,21 +39,40 @@ select nombre, apellido1||' '||apellido2 as apellidos from persona where tipo = 
 **/
 
 -- Devuelve el listado de los alumnos que nacieron en 1999.
-select distinct apellido1, apellido2, nombre from persona where tipo = 'alumno' and strftime('%Y', fecha_nacimiento) = '1999' order by apellido1, apellido2, nombre desc;
+select distinct apellido1, apellido2, nombre, fecha_nacimiento from persona where tipo = 'alumno' and fecha_nacimiento regexp '1999' order by apellido1, apellido2, nombre desc;
 /**
-
+┌───────────┬───────────┬─────────┬──────────────────┐
+│ apellido1 │ apellido2 │ nombre  │ fecha_nacimiento │
+├───────────┼───────────┼─────────┼──────────────────┤
+│ Domínguez │ Guerrero  │ Antonio │ 1999/02/11       │
+│ Strosin   │ Turcotte  │ Ismael  │ 1999/05/24       │
+└───────────┴───────────┴─────────┴──────────────────┘
 **/
 
 -- Devuelve el listado de profesores que no han dado de alta su número de teléfono en la base de datos y además su nif termina en K.
-select nombre, apellido1||' '||apellido2 as apellidos from persona where telefono is null and ;
+select nombre, apellido1||' '||apellido2 as apellidos, telefono, nif from persona where tipo = 'profesor' and telefono is null and nif regexp 'K$';
 /**
-
+┌───────────┬─────────────────┬──────────┬───────────┐
+│  nombre   │    apellidos    │ telefono │    nif    │
+├───────────┼─────────────────┼──────────┼───────────┤
+│ Antonio   │ Fahey Considine │          │ 10485008K │
+│ Guillermo │ Ruecker Upton   │          │ 85869555K │
+└───────────┴─────────────────┴──────────┴───────────┘
 **/
 
 -- Devuelve el listado de las asignaturas que se imparten en el primer cuatrimestre, en el tercer curso del grado que tiene el identificador 7.
-
+select nombre, cuatrimestre, curso, id_grado from asignatura where cuatrimestre = 1 and curso = 3 and id_grado = 7;
 /**
-
+┌───────────────────────────────────────────┬──────────────┬───────┬──────────┐
+│                  nombre                   │ cuatrimestre │ curso │ id_grado │
+├───────────────────────────────────────────┼──────────────┼───────┼──────────┤
+│ Bases moleculares del desarrollo vegetal  │ 1            │ 3     │ 7        │
+│ Fisiología animal                         │ 1            │ 3     │ 7        │
+│ Metabolismo y biosíntesis de biomoléculas │ 1            │ 3     │ 7        │
+│ Operaciones de separación                 │ 1            │ 3     │ 7        │
+│ Patología molecular de plantas            │ 1            │ 3     │ 7        │
+│ Técnicas instrumentales básicas           │ 1            │ 3     │ 7        │
+└───────────────────────────────────────────┴──────────────┴───────┴──────────┘
 **/
 
 -- Consultas multitabla (Join)
@@ -61,39 +80,181 @@ select nombre, apellido1||' '||apellido2 as apellidos from persona where telefon
 
 
 -- Devuelve un listado con los datos de todas las alumnas que se han matriculado alguna vez en el Grado en Ingeniería Informática (Plan 2015).
-
+select distinct p.nombre, apellido1||' '||apellido2 as apellidos, sexo, g.nombre as nombre_grado from persona p join alumno_se_matricula_asignatura ass on ass.id_alumno = p.id join asignatura a on ass.id_asignatura = a.id join grado g on a.id_grado = g.id where sexo = 'M' and id_grado = 4;
 /**
-
+┌────────┬────────────────────┬──────┬─────────────────────────────────────────────┐
+│ nombre │     apellidos      │ sexo │                nombre_grado                 │
+├────────┼────────────────────┼──────┼─────────────────────────────────────────────┤
+│ Sonia  │ Gea Ruiz           │ M    │ Grado en Ingeniería Informática (Plan 2015) │
+│ Irene  │ Hernández Martínez │ M    │ Grado en Ingeniería Informática (Plan 2015) │
+│ Inma   │ Lakin Yundt        │ M    │ Grado en Ingeniería Informática (Plan 2015) │
+└────────┴────────────────────┴──────┴─────────────────────────────────────────────┘
 **/
 
 -- Devuelve un listado con todas las asignaturas ofertadas en el Grado en Ingeniería Informática (Plan 2015).
-
+select nombre from asignatura where id_grado = 4;
 /**
-
+┌──────────────────────────────────────────────────────────────┐
+│                            nombre                            │
+├──────────────────────────────────────────────────────────────┤
+│ Álgegra lineal y matemática discreta                         │
+├──────────────────────────────────────────────────────────────┤
+│ Cálculo                                                      │
+├──────────────────────────────────────────────────────────────┤
+│ Física para informática                                      │
+├──────────────────────────────────────────────────────────────┤
+│ Introducción a la programación                               │
+├──────────────────────────────────────────────────────────────┤
+│ Organización y gestión de empresas                           │
+├──────────────────────────────────────────────────────────────┤
+│ Estadística                                                  │
+├──────────────────────────────────────────────────────────────┤
+│ Estructura y tecnología de computadores                      │
+├──────────────────────────────────────────────────────────────┤
+│ Fundamentos de electrónica                                   │
+├──────────────────────────────────────────────────────────────┤
+│ Lógica y algorítmica                                         │
+├──────────────────────────────────────────────────────────────┤
+│ Metodología de la programación                               │
+├──────────────────────────────────────────────────────────────┤
+│ Arquitectura de Computadores                                 │
+├──────────────────────────────────────────────────────────────┤
+│ Estructura de Datos y Algoritmos I                           │
+├──────────────────────────────────────────────────────────────┤
+│ Ingeniería del Software                                      │
+├──────────────────────────────────────────────────────────────┤
+│ Sistemas Inteligentes                                        │
+├──────────────────────────────────────────────────────────────┤
+│ Sistemas Operativos                                          │
+├──────────────────────────────────────────────────────────────┤
+│ Bases de Datos                                               │
+├──────────────────────────────────────────────────────────────┤
+│ Estructura de Datos y Algoritmos II                          │
+├──────────────────────────────────────────────────────────────┤
+│ Fundamentos de Redes de Computadores                         │
+├──────────────────────────────────────────────────────────────┤
+│ Planificación y Gestión de Proyectos Informáticos            │
+├──────────────────────────────────────────────────────────────┤
+│ Programación de Servicios Software                           │
+├──────────────────────────────────────────────────────────────┤
+│ Desarrollo de interfaces de usuario                          │
+├──────────────────────────────────────────────────────────────┤
+│ Ingeniería de Requisitos                                     │
+├──────────────────────────────────────────────────────────────┤
+│ Integración de las Tecnologías de la Información en las Orga │
+│ nizaciones                                                   │
+├──────────────────────────────────────────────────────────────┤
+│ Modelado y Diseño del Software 1                             │
+├──────────────────────────────────────────────────────────────┤
+│ Multiprocesadores                                            │
+├──────────────────────────────────────────────────────────────┤
+│ Seguridad y cumplimiento normativo                           │
+├──────────────────────────────────────────────────────────────┤
+│ Sistema de Información para las Organizaciones               │
+├──────────────────────────────────────────────────────────────┤
+│ Tecnologías web                                              │
+├──────────────────────────────────────────────────────────────┤
+│ Teoría de códigos y criptografía                             │
+├──────────────────────────────────────────────────────────────┤
+│ Administración de bases de datos                             │
+├──────────────────────────────────────────────────────────────┤
+│ Herramientas y Métodos de Ingeniería del Software            │
+├──────────────────────────────────────────────────────────────┤
+│ Informática industrial y robótica                            │
+├──────────────────────────────────────────────────────────────┤
+│ Ingeniería de Sistemas de Información                        │
+├──────────────────────────────────────────────────────────────┤
+│ Modelado y Diseño del Software 2                             │
+├──────────────────────────────────────────────────────────────┤
+│ Negocio Electrónico                                          │
+├──────────────────────────────────────────────────────────────┤
+│ Periféricos e interfaces                                     │
+├──────────────────────────────────────────────────────────────┤
+│ Sistemas de tiempo real                                      │
+├──────────────────────────────────────────────────────────────┤
+│ Tecnologías de acceso a red                                  │
+├──────────────────────────────────────────────────────────────┤
+│ Tratamiento digital de imágenes                              │
+├──────────────────────────────────────────────────────────────┤
+│ Administración de redes y sistemas operativos                │
+├──────────────────────────────────────────────────────────────┤
+│ Almacenes de Datos                                           │
+├──────────────────────────────────────────────────────────────┤
+│ Fiabilidad y Gestión de Riesgos                              │
+├──────────────────────────────────────────────────────────────┤
+│ Líneas de Productos Software                                 │
+├──────────────────────────────────────────────────────────────┤
+│ Procesos de Ingeniería del Software 1                        │
+├──────────────────────────────────────────────────────────────┤
+│ Tecnologías multimedia                                       │
+├──────────────────────────────────────────────────────────────┤
+│ Análisis y planificación de las TI                           │
+├──────────────────────────────────────────────────────────────┤
+│ Desarrollo Rápido de Aplicaciones                            │
+├──────────────────────────────────────────────────────────────┤
+│ Gestión de la Calidad y de la Innovación Tecnológica         │
+├──────────────────────────────────────────────────────────────┤
+│ Inteligencia del Negocio                                     │
+├──────────────────────────────────────────────────────────────┤
+│ Procesos de Ingeniería del Software 2                        │
+├──────────────────────────────────────────────────────────────┤
+│ Seguridad Informática                                        │
+└──────────────────────────────────────────────────────────────┘
 **/
 
 -- Devuelve un listado de los profesores junto con el nombre del departamento al que están vinculados. El listado debe devolver cuatro columnas, primer apellido, segundo apellido, nombre y nombre del departamento. El resultado estará ordenado alfabéticamente de menor a mayor por los apellidos y el nombre.
-
+select apellido1, apellido2, p.nombre, dp.nombre as nombre_departamento from persona p join profesor pr on p.id = pr.id_profesor join departamento dp on pr.id_departamento = dp.id order by apellido1, apellido2, p.nombre desc;
 /**
-
+┌────────────┬────────────┬───────────┬─────────────────────┐
+│ apellido1  │ apellido2  │  nombre   │ nombre_departamento │
+├────────────┼────────────┼───────────┼─────────────────────┤
+│ Fahey      │ Considine  │ Antonio   │ Economía y Empresa  │
+│ Hamill     │ Kozey      │ Manolo    │ Informática         │
+│ Kohler     │ Schoen     │ Alejandro │ Matemáticas         │
+│ Lemke      │ Rutherford │ Cristina  │ Economía y Empresa  │
+│ Monahan    │ Murray     │ Micaela   │ Agronomía           │
+│ Ramirez    │ Gea        │ Zoe       │ Informática         │
+│ Ruecker    │ Upton      │ Guillermo │ Educación           │
+│ Schmidt    │ Fisher     │ David     │ Matemáticas         │
+│ Schowalter │ Muller     │ Francesca │ Química y Física    │
+│ Spencer    │ Lakin      │ Esther    │ Educación           │
+│ Stiedemann │ Morissette │ Alfredo   │ Química y Física    │
+│ Streich    │ Hirthe     │ Carmen    │ Educación           │
+└────────────┴────────────┴───────────┴─────────────────────┘
 **/
 
 -- Devuelve un listado con el nombre de las asignaturas, año de inicio y año de fin del curso escolar del alumno con nif 26902806M.
-
+select a.nombre, anyo_inicio, anyo_fin, nif from asignatura a join alumno_se_matricula_asignatura ass on a.id = ass.id_asignatura join curso_escolar ce on ass.id_curso_escolar = ce.id join persona p on ass.id_alumno = p.id where nif = '26902806M';
 /**
-
+┌──────────────────────────────────────┬─────────────┬──────────┬───────────┐
+│                nombre                │ anyo_inicio │ anyo_fin │    nif    │
+├──────────────────────────────────────┼─────────────┼──────────┼───────────┤
+│ Álgegra lineal y matemática discreta │ 2014        │ 2015     │ 26902806M │
+│ Cálculo                              │ 2014        │ 2015     │ 26902806M │
+│ Física para informática              │ 2014        │ 2015     │ 26902806M │
+└──────────────────────────────────────┴─────────────┴──────────┴───────────┘
 **/
 
 -- Devuelve un listado con el nombre de todos los departamentos que tienen profesores que imparten alguna asignatura en el Grado en Ingeniería Informática (Plan 2015).
-
+select distinct dp.nombre from departamento dp join profesor pr on dp.id = pr.id_departamento join asignatura a on pr.id_profesor = a.id_profesor where id_grado = 4;
 /**
-
+┌─────────────┐
+│   nombre    │
+├─────────────┤
+│ Informática │
+└─────────────┘
 **/
 
 -- Devuelve un listado con todos los alumnos que se han matriculado en alguna asignatura durante el curso escolar 2018/2019.
-
+select distinct p.nombre, apellido1||' '||apellido2 as apellidos from persona p join alumno_se_matricula_asignatura ass on p.id = ass.id_alumno join curso_escolar ce on ass.id_curso_escolar = ce.id where anyo_inicio in (2018, 2019) or anyo_fin in (2018, 2019);
 /**
-
+┌────────┬────────────────────┐
+│ nombre │     apellidos      │
+├────────┼────────────────────┤
+│ Sonia  │ Gea Ruiz           │
+│ Irene  │ Hernández Martínez │
+│ Inma   │ Lakin Yundt        │
+└────────┴────────────────────┘
 **/
 
 -- Consultas multitabla (Where)
@@ -101,9 +262,24 @@ select nombre, apellido1||' '||apellido2 as apellidos from persona where telefon
 
 
 -- Devuelve un listado con los nombres de todos los profesores y los departamentos que tienen vinculados. El listado también debe mostrar aquellos profesores que no tienen ningún departamento asociado. El listado debe devolver cuatro columnas, nombre del departamento, primer apellido, segundo apellido y nombre del profesor. El resultado estará ordenado alfabéticamente de menor a mayor por el nombre del departamento, apellidos y el nombre.
-
+select dp.nombre as nombre_departamento, apellido1, apellido2, p.nombre from persona p, departamento dp, profesor pr where p.id = pr.id_profesor and (pr.id_departamento = dp.id or pr.id_departamento is null) order by dp.nombre asc, apellido1 asc, apellido2 asc, p.nombre asc;
 /**
-
+┌─────────────────────┬────────────┬────────────┬───────────┐
+│ nombre_departamento │ apellido1  │ apellido2  │  nombre   │
+├─────────────────────┼────────────┼────────────┼───────────┤
+│ Agronomía           │ Monahan    │ Murray     │ Micaela   │
+│ Economía y Empresa  │ Fahey      │ Considine  │ Antonio   │
+│ Economía y Empresa  │ Lemke      │ Rutherford │ Cristina  │
+│ Educación           │ Ruecker    │ Upton      │ Guillermo │
+│ Educación           │ Spencer    │ Lakin      │ Esther    │
+│ Educación           │ Streich    │ Hirthe     │ Carmen    │
+│ Informática         │ Hamill     │ Kozey      │ Manolo    │
+│ Informática         │ Ramirez    │ Gea        │ Zoe       │
+│ Matemáticas         │ Kohler     │ Schoen     │ Alejandro │
+│ Matemáticas         │ Schmidt    │ Fisher     │ David     │
+│ Química y Física    │ Schowalter │ Muller     │ Francesca │
+│ Química y Física    │ Stiedemann │ Morissette │ Alfredo   │
+└─────────────────────┴────────────┴────────────┴───────────┘
 **/
 
 -- Devuelve un listado con los profesores que no están asociados a un departamento.
